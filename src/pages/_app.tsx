8ab54cd0bba5b80cb26eb3b0616681fa3dc47f5b
rikusen0335 <rikusen0335@gmail.com>
2021-01-11 15:20:07 +0900

    fix: i think I'm done with this (nop)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import type { AppProps } from 'next/app';
-import './index.css';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
